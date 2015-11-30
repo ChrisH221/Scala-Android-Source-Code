e5d1a7ffb5653a0fdf5f083f0dea3e50f96fa6c4
ChrisH221 <C.Howell2@uni.brighton.ac.uk>
2015-11-30 14:01:09 +0000

    Server + SBTdir

---
+++ project/Client/app/src/main/java/client/akka/chris/client_android/MainActivity.scala
@@ -0,0 +1,14 @@
+package client.akka.chris.client_android
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by chris on 30/11/15.
+ */
+class MainActivity extends Activity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_activity)
+  }
+}
