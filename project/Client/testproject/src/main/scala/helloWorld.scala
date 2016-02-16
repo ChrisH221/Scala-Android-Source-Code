0c54286d831b13f91a5b2408b2aa917abb128e46
Chris <C.Howell2@uni.brighton.ac.uk>
2016-02-16 10:15:04 +0000

    Actor testing
    Started testing actor message passing

--- project/Client/testproject/src/main/scala/helloWorld.scala
+++ project/Client/testproject/src/main/scala/helloWorld.scala
@@ -12,31 +12,41 @@ case class received(text:String) extends Message
 
 class helloWorld extends Activity with TypedActivity with helpers{
   override def onCreate(bundle: Bundle) {
+  
     super.onCreate(bundle)
     setContentView(R.layout.main)
 	val x = new send("hello did you get my message")
 	val system = ActorSystem("client-akka")
+	val helloActor = system.actorOf(Props(new actorM()), name = "helloactor")
 	val button = findView(TR.button1)
 	button.setOnClickListener((v : View) => {
 	
-	//val r = new read_write("data/newFile.txt") 
-	//r.test("hey")
-	findView(TR.textview).setText("")
-	val h = new handler
-	//val r = new read_write("hey")
-	//val result = r.test("hi")
-	val editText = findView(TR.editTextResult)
-	findView(TR.textview).setText(h.encode(editText.getText().toString())._1 mkString)
+		findView(TR.textview).setText("")
+		helloActor ! "change"
+
 
     })
-	//Button = (Button) findViewById(R.id.button1);
-	//val y = new received("hello did you get my message")
-//	findView(TR.textview).setText(add1(x))
-	//findView(TR.textview2).setText(add1(y))
   }
 
 
+class actorM extends Actor {
+   def receive = {
+	
+	case "change" => changeUI()
+	
+   
+   
+   
+   }
+   
+   }
 
+def changeUI(){
 
+	val h = new handler
+	val editText = findView(TR.editTextResult)
+	findView(TR.textview).setText(h.encode(editText.getText().toString())._1 mkString)
+
+}
 
 }
